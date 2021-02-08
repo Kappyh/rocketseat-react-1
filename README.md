76e9d515de5f7a13c1e933f8e5033e3f94061380
Gabriela Mendes <contato@gabrielamendes.dev>
2021-02-07 21:41:45 -0300

    adiciona axios, lista projetos do backend

---
+++ README.md
@@ -0,0 +1,12 @@
+# Project with basic React
+
+Install dependencies 
+
+`yarn`
+
+Run project
+`yarn dev`
+
+## Application backend
+
+Check it out [this](https://github.com/Kappyh/rocketseat-backend-1/blob/master/package.json) project, clone it and follow the same steps above to use it.
--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import './App.css';
-import backgroundImage from './assets/background.jpg';
+// import backgroundImage from './assets/background.jpg';
+import api from './services/api';
 
 function App() {
-    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front end web']);
+    const [projects, setProjects] = useState([]);
 
     function handleAddProject() {
         setProjects([...projects, `New project ${Date.now()}`]);
     }
+
+
+
+    useEffect(()=>{
+        api.get('projects').then(response=>{
+            setProjects(response.data);
+        })
+    },[])
+
     return (
         <>
             <Header title="Homepage" />
-            <img width="300" src={backgroundImage} />
+            {/*  <img width="300" src={backgroundImage} /> */}
             <ul>
-                {projects.map((project, index) => <li key={index}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
             <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
         </>
